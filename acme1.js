8581abe58bc1626422e1ddea1b4b211a0c22f449
Robert Albenda <ralbenda@gmail.com>
2022-01-24 15:26:29 -0500

    first commit

---
+++ acme1.js
@@ -0,0 +1,61 @@
+const employees = [
+    { id: 1, name: 'moe'},
+    { id: 2, name: 'larry', managerId: 1},
+    { id: 4, name: 'shep', managerId: 2},
+    { id: 3, name: 'curly', managerId: 1},
+    { id: 5, name: 'groucho', managerId: 3},
+    { id: 6, name: 'harpo', managerId: 5},
+    { id: 8, name: 'shep Jr.', managerId: 4},
+    { id: 99, name: 'lucy', managerId: 1}
+  ];
+  
+  const spacer = (text)=> {
+    if(!text){
+      return console.log('');
+    }
+    const stars = new Array(5).fill('*').join('');
+    console.log(`${stars} ${text} ${stars}`);
+  }
+  
+  spacer('findEmployeeByName Moe')
+  // given a name and array of employees, return employee
+  function findEmployeeByName(employee,array) {
+    for(let i =0; i <array.length; i++) {
+      if(array[i]['name']===employee) {
+        console.log(array[i])
+      }
+    }
+  
+  }
+  console.log(findEmployeeByName('moe', employees));//{ id: 1, name: 'moe' }
+  spacer('')
+  
+  spacer('findManagerFor Shep Jr.')
+  //given an employee and a list of employees, return the employee who is the manager
+  console.log(findManagerFor(findEmployeeByName('shep Jr.', employees), employees));//{ id: 4, name: 'shep', managerId: 2 }
+  spacer('')
+  
+  spacer('findCoworkersFor Larry')
+  
+  //given an employee and a list of employees, return the employees who report to the same manager
+  console.log(findCoworkersFor(findEmployeeByName('larry', employees), employees));/*
+  [ { id: 3, name: 'curly', managerId: 1 },
+    { id: 99, name: 'lucy', managerId: 1 } ]
+  */
+  
+  spacer('');
+  
+  spacer('findManagementChain for moe')
+  //given an employee and a list of employees, return a the management chain for that employee. The management chain starts from the employee with no manager with the passed in employees manager 
+  console.log(findManagementChainForEmployee(findEmployeeByName('moe', employees), employees));//[  ]
+  spacer('');
+  
+  spacer('findManagementChain for shep Jr.')
+  console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employees), employees));/*
+  [ { id: 1, name: 'moe' },
+    { id: 2, name: 'larry', managerId: 1 },
+    { id: 4, name: 'shep', managerId: 2 }]
+  */
+  spacer('');
+  
+  
