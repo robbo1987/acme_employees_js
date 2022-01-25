5c3eb04c33e08031a5bec2a915db51fbebf80ad5
Robert Albenda <ralbenda@gmail.com>
2022-01-25 16:15:19 -0500

    final commit

--- acme1.js
+++ acme1.js
@@ -75,3 +75,105 @@ const employees = [
   spacer('');
   
   
+  spacer('generateManagementTree')
+  //this is the solution posted in slack, I wasnt able to get the answer but wanted this solution for my records//
+  const generateManagementTree = employees => {
+    let boss;
+    //finds the boss of the company
+    for (let i in employees) {
+      if (!employees[i].managerId) {
+        boss = employees[i];
+      }
+    }
+  
+    //assigns reports property to the boss and call the helper function to find the next people down the chain
+    boss.reports = findMinionsForManager(employees, boss);
+    
+    //after all the iterations of the helper functions, assigns the org chart to 'companyHierarchy'
+    const companyHierarchy = boss;
+    return companyHierarchy;
+  }
+  
+  const findMinionsForManager = (employees, manager) => {
+    const subtree = [];
+    employees
+    //filters employees who report to the same manager
+    .filter(person => person.managerId === manager.id)
+  
+    //assigns reports property to each employee and recursively calls the function to find their minions
+    .forEach( person => {
+      person.reports = findMinionsForManager(employees, person);
+      return subtree.push(person);
+    })
+    return subtree;
+  }
+//given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
+console.log(JSON.stringify(generateManagementTree(employees), null, 2));
+/*
+{
+  "id": 1,
+  "name": "moe",
+  "reports": [
+    {
+      "id": 2,
+      "name": "larry",
+      "managerId": 1,
+      "reports": [
+        {
+          "id": 4,
+          "name": "shep",
+          "managerId": 2,
+          "reports": [
+            {
+              "id": 8,
+              "name": "shep Jr.",
+              "managerId": 4,
+              "reports": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "curly",
+      "managerId": 1,
+      "reports": [
+        {
+          "id": 5,
+          "name": "groucho",
+          "managerId": 3,
+          "reports": [
+            {
+              "id": 6,
+              "name": "harpo",
+              "managerId": 5,
+              "reports": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 99,
+      "name": "lucy",
+      "managerId": 1,
+      "reports": []
+    }
+  ]
+}
+*/
+spacer('');
+
+spacer('displayManagementTree')
+//given a tree of employees, generate a display which displays the hierarchy
+displayManagementTree(generateManagementTree(employees));/*
+moe
+-larry
+--shep
+---shep Jr.
+-curly
+--groucho
+---harpo
+-lucy
+*/
